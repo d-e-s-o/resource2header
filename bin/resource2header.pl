d71c707243699e902e60583b276d1ccc1fb23e5c
Daniel Mueller <deso@posteo.net>
2012-11-17 00:04:27 -0800

    Add resource2header Perl script
    This change introduces the resource2header script. This script can be
    used to convert an arbitrary binary file into a C/C++ header file
    containing a variable with this file's data and another one representing
    its size.
    This header can be used to embedd arbitrary binary data statically
    into a program, which is helpful if no external dependencies are to be
    shipped or if opening external files is not an option, for example.

---
+++ bin/resource2header.pl
@@ -0,0 +1,97 @@
+#!/usr/bin/perl -w
+
+#/***************************************************************************
+# *   Copyright (C) 2012 Daniel Mueller (deso@posteo.net)                   *
+# *                                                                         *
+# *   This program is free software: you can redistribute it and/or modify  *
+# *   it under the terms of the GNU General Public License as published by  *
+# *   the Free Software Foundation, either version 3 of the License, or     *
+# *   (at your option) any later version.                                   *
+# *                                                                         *
+# *   This program is distributed in the hope that it will be useful,       *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU General Public License for more details.                          *
+# *                                                                         *
+# *   You should have received a copy of the GNU General Public License     *
+# *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+# ***************************************************************************/
+
+
+use warnings;
+use strict;
+
+use File::Basename;
+use File::Spec;
+
+
+my $argc = scalar(@ARGV);
+
+if (!($argc == 1 || $argc == 2))
+{
+  print "invalid number of arguments\n";
+  print "usage: $ARGV[0] <input file> [<output dir>]\n";
+  exit -1;
+}
+
+my $input = "$ARGV[0]";
+
+if (! -f "$input")
+{
+  print "invalid file given\n";
+  exit -2;
+}
+
+open(FILE, $input) or die($!);
+binmode(FILE);
+
+my $text   = "";
+my $length = 0;
+my $data;
+
+while (read(FILE, $data, 1) > 0)
+{
+  $text   .= ", " if $text ne "";
+  $text   .= "0x" . sprintf("%02X", ord($data));
+  $length += 1;
+}
+
+while ($text =~ s/([^\n]{76}), /  $1,\n/g) {}
+$text =~ s/([^\n]{1,76})$/  $1/g;
+
+close(FILE);
+
+# we are only interested in the actual filename, no paths and stuff
+my $output = basename($input);
+
+my $temp = lc($output);
+   $temp =~ tr/.-/_/;
+
+my $guard   = uc($temp) . "_HPP";
+my $content =    $temp  . "_data";
+my $size    =    $temp  . "_size";
+my $file    = $output   . ".hpp";
+
+if ($argc == 2)
+{
+  $file = File::Spec->catfile("$ARGV[1]", "$file");
+}
+
+open(FILE, ">$file") or die($!);
+printf FILE<<EOF;
+// $file
+
+#ifndef $guard
+#define $guard
+
+
+unsigned char const ${content}[] = {
+$text};
+
+unsigned int const $size = $length;
+
+
+#endif
+EOF
+
+close(FILE);
